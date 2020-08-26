525a4cf963f8151b4c6abd1140bbbf37e5b19725
JoscaWij <Josca.W@gmx.net>
2020-08-26 16:36:01 +0200

    Use mongoDB instead of password.json

--- cli.js
+++ cli.js
@@ -18,38 +18,50 @@ const {
 
 const { encrypt, decrypt, createHash, verifyHash } = require("./lib/crypto");
 
+const { MongoClient } = require("mongodb");
+const uri =
+  "mongodb+srv://JoscaWij:%23Development%232020@development.2unwm.mongodb.net?retryWrites=true&w=majority";
+const client = new MongoClient(uri);
+
 async function main() {
-  const originalMasterPassword = await readMasterPassword();
-  if (!originalMasterPassword) {
-    const { newMasterPassword } = await askForNewMasterPassword();
-    const encrypedMasterPassword = createHash(newMasterPassword);
-    await writeMasterPassword(encrypedMasterPassword);
-    console.log("Master Password set!");
-    return;
-  }
+  try {
+    await client.connect();
+    const database = client.db("password_key");
 
-  const { masterPassword, task } = await askStartQuestions();
-
-  if (!verifyHash(masterPassword, originalMasterPassword)) {
-    console.log("Master Password is incorrect!");
-    return;
-  }
-
-  console.log("Master password is correct!");
-  if (task === CHOICE_GET) {
-    const { key } = await askGetQuestions();
-    try {
-      const password = await readPassword(key);
-      const dycriptedPassword = decrypt(password, masterPassword);
-      console.log(`Your ${key} password is ${dycriptedPassword}`);
-    } catch (error) {
-      console.error("Something went wrong");
+    const originalMasterPassword = await readMasterPassword();
+    if (!originalMasterPassword) {
+      const { newMasterPassword } = await askForNewMasterPassword();
+      const encrypedMasterPassword = createHash(newMasterPassword);
+      await writeMasterPassword(encrypedMasterPassword);
+      console.log("Master Password set!");
+      return;
     }
-  } else if (task === CHOICE_SET) {
-    const { service, newpassword } = await askSetQuestions();
-    const encryptedPassword = encrypt(newpassword, masterPassword);
-    await writePassword(service, encryptedPassword);
-    console.log(`New Password for ${service} set`);
+
+    const { masterPassword, task } = await askStartQuestions();
+
+    if (!verifyHash(masterPassword, originalMasterPassword)) {
+      console.log("Master Password is incorrect!");
+      return;
+    }
+
+    console.log("Master password is correct!");
+    if (task === CHOICE_GET) {
+      const { key } = await askGetQuestions();
+      try {
+        const password = await readPassword(key, database);
+        const dycriptedPassword = decrypt(password, masterPassword);
+        console.log(`Your ${key} password is ${dycriptedPassword}`);
+      } catch (error) {
+        console.error("Something went wrong");
+      }
+    } else if (task === CHOICE_SET) {
+      const { service, newpassword } = await askSetQuestions();
+      const encryptedPassword = encrypt(newpassword, masterPassword);
+      await writePassword(service, encryptedPassword, database);
+      console.log(`New Password for ${service} set`);
+    }
+  } finally {
+    await client.close();
   }
 }
 
