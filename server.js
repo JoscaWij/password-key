2611e17b1ecaccf73bc3e11da61d74994f2a195b
JoscaWij <Josca.W@gmx.net>
2020-08-27 16:03:53 +0200

    App post request

--- server.js
+++ server.js
@@ -2,23 +2,33 @@ const MongoClient = require("mongodb").MongoClient;
 require("dotenv").config();
 
 const express = require("express");
-const { readPassword } = require("./lib/passwords");
-const { decrypt } = require("./lib/crypto");
+const bodyParser = require("body-parser");
+const { readPassword, writePassword } = require("./lib/passwords");
+const { decrypt, encrypt } = require("./lib/crypto");
 const port = 3000;
 const app = express();
 
 const client = new MongoClient(process.env.MONGO_URI);
 
-const masterPassword = "123";
+const masterPassword = process.env.MASTER_PASSWORD;
 
 async function server() {
   await client.connect();
   console.log("Connected to Database");
   const database = client.db(process.env.MONGO_DB_NAME);
 
+  app.use(bodyParser.json());
+
+  app.post("/api/passwords", async (request, response) => {
+    const { name, value } = request.body;
+    const encryptedPassword = encrypt(value, masterPassword);
+    await writePassword(name, encryptedPassword, database);
+    response.send(`New password for ${name} set`);
+  });
+
   app.get("/api/passwords/:name", async (request, response) => {
-    const key = request.params.name;
-    const encryptedPassword = await readPassword(key, database);
+    const { name } = request.params.name;
+    const encryptedPassword = await readPassword(name, database);
     const password = decrypt(encryptedPassword, masterPassword);
     response.send(password);
   });
