e62bd89c3fa166cda18d625c5db5483decc53c5e
JoscaWij <Josca.W@gmx.net>
2020-08-27 14:12:57 +0200

    Get specific passwords from api

--- server.js
+++ server.js
@@ -2,39 +2,30 @@ const MongoClient = require("mongodb").MongoClient;
 require("dotenv").config();
 
 const express = require("express");
+const { readPassword } = require("./lib/passwords");
+const { decrypt } = require("./lib/crypto");
 const port = 3000;
-const { db } = require("mongodb");
 const app = express();
 
 const client = new MongoClient(process.env.MONGO_URI);
 
-app.get("api/passwords/wifi", (request, response) => {
-  response.send("123");
-});
-
-app.post("/api/passwords", (request, response) => {
-  response.send("passwords");
-});
+const masterPassword = "123";
 
 async function server() {
-  await client.connect({
-    useUnifiedTopology: true,
-  });
-
-  app.get("/", async (request, response) => {
-    const result = await db.collection("passwords").find().toArray();
-    response.send(result);
-  });
-
+  await client.connect();
   console.log("Connected to Database");
-  const db = client.db(process.env.MONGO_DB_NAME);
-}
+  const database = client.db(process.env.MONGO_DB_NAME);
 
-/*
-.then((client) => {
-    
-  .catch((error) => console.error(error)); */
+  app.get("/api/passwords/:name", async (request, response) => {
+    const key = request.params.name;
+    const encryptedPassword = await readPassword(key, database);
+    const password = decrypt(encryptedPassword, masterPassword);
+    response.send(password);
+  });
+}
 
 app.listen(port, () => {
   console.log(`listening on ${port}`);
 });
+
+server();
