d2f88a3ab37d82166d3783a9aa8416b811047035
JoscaWij <Josca.W@gmx.net>
2020-08-27 13:38:38 +0200

    Install nodemon and write script

--- server.js
+++ server.js
@@ -6,22 +6,33 @@ const port = 3000;
 const { db } = require("mongodb");
 const app = express();
 
-/* MongoClient.connect(process.env.MONGO_URI, {
+const client = new MongoClient(process.env.MONGO_URI);
+
+app.get("api/passwords/wifi", (request, response) => {
+  response.send("123");
+});
+
+app.post("/api/passwords", (request, response) => {
+  response.send("passwords");
+});
+
+async function server() {
+  await client.connect({
     useUnifiedTopology: true,
-})
+  });
+
+  app.get("/", async (request, response) => {
+    const result = await db.collection("passwords").find().toArray();
+    response.send(result);
+  });
+
+  console.log("Connected to Database");
+  const db = client.db(process.env.MONGO_DB_NAME);
+}
+
+/*
 .then((client) => {
-    console.log("Connected to Database");
-    const db = client.db(process.env.MONGO_DB_NAME);
     
-    app.get("/", (request, response) => {
-      db.collection("passwords")
-        .find()
-        .toArray()
-        .then((results) => {
-            console.log(results);
-        });
-    });
-  })
   .catch((error) => console.error(error)); */
 
 app.listen(port, () => {
