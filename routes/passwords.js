f61bce78a4055999677a389bb3d2b750e7b58cf3
JoscaWij <Josca.W@gmx.net>
2020-08-31 10:25:12 +0200

    Prepare passwords and server.js

--- routes/passwords.js
+++ routes/passwords.js
@@ -1,41 +1,8 @@
-const MongoClient = require("mongodb").MongoClient;
-require("dotenv").config();
-
 const express = require("express");
-const bodyParser = require("body-parser");
-const {
-  readPassword,
-  writePassword,
-  readAllPasswords,
-} = require("./lib/passwords");
-const { decrypt, encrypt } = require("./lib/crypto");
-const port = 3000;
+
 const app = express();
 
-const client = new MongoClient(process.env.MONGO_URI, {
-  useUnifiedTopology: true,
-});
-
-const masterPassword = process.env.MASTER_PASSWORD;
-
-async function server() {
-  try {
-    await client.connect();
-    console.log("Connected to Database");
-  } catch (error) {
-    console.log("MongoDB is a Mongo today");
-    console.error(error);
-  }
-  const database = client.db(process.env.MONGO_DB_NAME);
-
-  app.use(bodyParser.json());
-
-  app.use((request, response, next) => {
-    console.log(`Request ${request.method} on ${request.url}`);
-    next();
-  });
-
-/*   app.get("/api/passwords/:name", async (request, response) => {
+/* app.get("/api/passwords/:name", async (request, response) => {
     try {
       const { name } = request.params;
       const encryptedPassword = await readPassword(name, database);
@@ -69,8 +36,3 @@ async function server() {
     }
   });
 } */
-
-app.listen(port, () => {
-  console.log(`listening on ${port}`);
-}),
-  server();
