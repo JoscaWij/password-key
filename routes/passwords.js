424586f70ee8448be6d18a04f3b46419e77d81c5
JoscaWij <Josca.W@gmx.net>
2020-09-01 10:53:21 +0200

    Delete comments

--- routes/passwords.js
+++ routes/passwords.js
@@ -58,38 +58,3 @@ function createPasswordsRouter(database, masterPassword) {
 module.exports = {
   createPasswordsRouter,
 };
-
-/* app.get("/api/passwords/:name", async (request, response) => {
-    try {
-      const { name } = request.params;
-      const encryptedPassword = await readPassword(name, database);
-      const password = decrypt(encryptedPassword, masterPassword);
-      response.send(password);
-    } catch (error) {
-      console.log("Couldn't get data from MongoDB"), console.error(error);
-    }
-  });
-
-  app.post("/api/passwords", async (request, response) => {
-    try {
-      const { name, value } = request.body;
-      const encryptedPassword = encrypt(value, masterPassword);
-      await writePassword(name, encryptedPassword, database);
-      response.send(`New password for ${name} set`);
-    } catch (error) {
-      console.log("Couldn't post data to MongoDB-database"),
-        console.error(error);
-    }
-  });
-
-  app.get("/api/passwords/all", async (request, response) => {
-    try {
-      const encryptedPasswords = await readAllPasswords(database);
-      const passwords = decrypt(encryptedPasswords, masterPassword);
-      response.send(passwords);
-    } catch (error) {
-      console.log("Couldn't get data from MongoDB. To much data"),
-        console.error(error);
-    }
-  });
-} */
